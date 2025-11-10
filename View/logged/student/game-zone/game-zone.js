a240f54bda2006202b695959ed3a610d1c22b8f4
manuela92 <manuela92araujo@gmail.com>
2025-11-10 10:53:38 -0300

    Instituição: ajustes no css da página add-profile; Aluno: inclusão do jogo Forca (agora temos 2 jogos) e conexão dos jogos na tela de Games

--- View/logged/student/game-zone/game-zone.js
+++ View/logged/student/game-zone/game-zone.js
@@ -2,6 +2,6 @@ navigateBack = () => {
     window.location.href = '../student-home/student-home.html';
 }
 
-navigateGame = () => {
-    window.location.href = 'game/game.html';
+function navigateGame(page) {
+  window.location.href = page;
 }
