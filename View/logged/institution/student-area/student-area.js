a5cde71cf97e3878cf5e1d86e859f9aa04019be7
gui07777 <guilherme2006.gmc@gmail.com>
2025-11-13 21:48:01 -0300

    att

--- View/logged/institution/student-area/student-area.js
+++ View/logged/institution/student-area/student-area.js
@@ -1,4 +1,26 @@
-function showStudentDetails() {
-    let closedModal = document.querySelector('#student-informations-modal')
-    closedModal.classList.add('show-informations')
+const modal = document.querySelector('#information-modal-hide');
+const btnVisualizar = document.querySelectorAll('.btn-visualizar');
+const overlayHide = document.querySelector('#overlay-hide');
+
+btnVisualizar.forEach(btn => {
+  btn.addEventListener('click', () => {
+    viewActivitie();
+  });
+});
+
+function viewActivitie() {
+  if (!modal && !overlayHide) return;
+  overlayHide.classList.add('overlay');
+  overlayHide.style.display = 'flex';
+  modal.classList.add('information-modal');
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  if (modal) {
+    modal.classList.remove('information-modal');
+    modal.style.display = 'none';
+    overlayHide.classList.remove('overlay');
+    overlayHide.style.display = 'none';
+  }
 }
--- View/logged/teacher/activities-delivered/activities-delivered.js
+++ View/logged/teacher/activities-delivered/activities-delivered.js
@@ -1,5 +1,26 @@
-document.querySelectorAll('.btn-visualizar').forEach(btn => {
+const modal = document.querySelector('#activities-delivered-modal-hide');
+const btnVisualizar = document.querySelectorAll('.btn-visualizar');
+const overlayHide = document.querySelector('#overlay-hide');
+
+btnVisualizar.forEach(btn => {
   btn.addEventListener('click', () => {
-    alert('Visualizando entrega...');
+    viewActivitie();
   });
 });
+
+function viewActivitie() {
+  if (!modal && !overlayHide) return;
+  overlayHide.classList.add('overlay');
+  overlayHide.style.display = 'flex';
+  modal.classList.add('activities-delivered-modal');
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  if (modal) {
+    modal.classList.remove('activities-delivered-modal');
+    modal.style.display = 'none';
+    overlayHide.classList.remove('overlay');
+    overlayHide.style.display = 'none';
+  }
+}
