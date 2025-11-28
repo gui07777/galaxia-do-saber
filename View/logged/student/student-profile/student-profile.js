63f4034f43ddc6e82066c079f224a819fe1c2fc1
LevineoS <levisantosbr5@gmail.com>
2025-11-27 23:41:19 -0300

    Tela de perfil do aluno arrumada.

--- View/logged/student/student-profile/student-profile.js
+++ View/logged/student/student-profile/student-profile.js
@@ -11,18 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("nome").value = data.nome || "";
             document.getElementById("email").value = data.email || "";
 
-            if (data.data_nasc) {
-                const partes = data.data_nasc.split("/");
-                if (partes.length === 3) {
-                    const dataISO = `${partes[2]}-${partes[1]}-${partes[0]}`; 
-                    document.getElementById("data_nasc").value = dataISO;
-                }
-            }
+            document.getElementById("data_nasc").value = data.data_nasc || "";
 
             document.getElementById("turma").value = data.turma_nome || "";
         })
         .catch(err => console.error("Erro ao carregar dados do aluno:", err));
 });
+
 
 function navigationBack() {
     window.location.href = "../../student/student-home/student-home.php";
